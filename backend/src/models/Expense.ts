8908cb2ef088749058bdee0765b03a11dd6d3a5f
Edu Moreno <e.m.morenolp@gmail.com>
2025-08-01 16:10:42 -0300

    implemetacion models

---
+++ backend/src/models/Expense.ts
@@ -0,0 +1,60 @@
+// src/models/Expense.ts
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "../config/database";
+
+export interface ExpenseAttributes {
+  id?: number;
+  currency: string;
+  amount: number;
+  date: string;
+  description: string;
+  provider: string;
+}
+
+export class Expense
+  extends Model<ExpenseAttributes>
+  implements ExpenseAttributes
+{
+  public id!: number;
+  public currency!: string;
+  public amount!: number;
+  public date!: string;
+  public description!: string;
+  public provider!: string;
+}
+
+Expense.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    currency: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    provider: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Expense",
+    tableName: "expenses",
+    timestamps: false,
+  }
+);
---
+++ backend/src/models/Sale.ts
@@ -0,0 +1,64 @@
+// src/models/Sale.ts
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "../config/database";
+
+export interface SaleAttributes {
+  id?: number;
+  id_venta: string;
+  currency: string;
+  amount: number;
+  date: string;
+  description: string;
+  client: string;
+}
+
+export class Sale extends Model<SaleAttributes> implements SaleAttributes {
+  public id!: number;
+  public id_venta!: string;
+  public currency!: string;
+  public amount!: number;
+  public date!: string;
+  public description!: string;
+  public client!: string;
+}
+
+Sale.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    id_venta: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    currency: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    client: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Sale",
+    tableName: "sales",
+    timestamps: false,
+  }
+);
